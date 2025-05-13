0697f34deb1943a65be5654e7bf501b1196cc64b
강태빈 <ktbisboy99@gmail.com>
2025-05-13 20:34:16 +0900

    fix : 엔티티 어노테이션 추가

--- src/main/java/org/example/backend/fishbook/entity/FishBook.java
+++ src/main/java/org/example/backend/fishbook/entity/FishBook.java
@@ -2,9 +2,11 @@ package org.example.backend.fishbook.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class FishBook {
 
     @Id
@@ -19,4 +21,10 @@ public class FishBook {
 
     @Column(nullable = false)
     private String description;
+
+    public FishBook(String name, String habitat, String description) {
+        this.name = name;
+        this.habitat = habitat;
+        this.description = description;
+    }
 }
--- src/main/java/org/example/backend/fishbook/entity/UserFish.java
+++ src/main/java/org/example/backend/fishbook/entity/UserFish.java
@@ -2,20 +2,25 @@ package org.example.backend.fishbook.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.example.backend.user.entity.User;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class UserFish {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Setter
     private boolean caught;
 
+    @Setter
     private LocalDateTime caughtAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -25,4 +30,9 @@ public class UserFish {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "fish_book_id")
     private FishBook fishBook;
+
+    public UserFish(User user, FishBook fishBook) {
+        this.user = user;
+        this.fishBook = fishBook;
+    }
 }
