87505d971bc400673ef1dad8b9767cfe66f13383
dang-woo <kdw3917ss@gmail.com>
2025-05-13 22:36:43 +0900

    1.컨트롤러 뮬고기 도감 전체조회,상세조회 추가 2. 시큐리티컨피그 파일 28번째줄  "/api/fishbook/**" 추가 3.UserFishRepository 에서 특정 사용자의 특정 물고기 조회 및 caught 상태 추가 4. UserFishService에서 물고기 도감 상세조회 서비스 코드 추가
    물고기 도감 전체 조회 와 물고기 도감 상세 조회 기능 은 완성했습니다 물고기 도감 해금 기능은 미구현

--- src/main/java/org/example/backend/fishbook/controller/FishBookController.java
+++ src/main/java/org/example/backend/fishbook/controller/FishBookController.java
@@ -1,11 +1,15 @@
 package org.example.backend.fishbook.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.fishbook.dto.FishBookResponse;
+import org.example.backend.fishbook.entity.FishBook;
 import org.example.backend.fishbook.repository.FishBookRepository;
 import org.example.backend.fishbook.repository.UserFishRepository;
 import org.example.backend.fishbook.service.UserFishService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/fishbook")
@@ -16,5 +20,20 @@ public class FishBookController {
     private final FishBookRepository fishBookRepository;
     private final UserFishRepository userFishRepository;
 
+    // 1. 물고기 도감 전체 조회
+    @GetMapping
+    public ResponseEntity<List<FishBookResponse>> getFishBook(@RequestParam Long userId) {
+        List<FishBookResponse> fishBook = userFishService.getUserFishBook(userId);
+        return ResponseEntity.ok(fishBook);
+    }
+
+    // 2. 물고기 도감 상세 조회
+    @GetMapping("/{fishId}")
+    public ResponseEntity<FishBook> getFishDetails(
+            @PathVariable Long fishId,
+            @RequestParam Long userId) {
+        FishBook fishDetails = userFishService.getFishDetails(userId, fishId);
+        return ResponseEntity.ok(fishDetails);
+    }
 
 }
